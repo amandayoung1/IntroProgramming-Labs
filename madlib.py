73bac7fd879238db5a0a8710f39d4b94d8fc9bf3
amandayoung1 <amanda.young@marist.edu>
2018-09-13 14:28:42 -0400

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,19 @@
+# Intro to Programming
+# Author: Amanda Young
+# Date: 9/13/2018
+# Mad Lib Program for Lab Exercise
+
+def main ():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adjective = input("Enter an adjective: ")
+    place = input("Enter a place: ")
+    print("\n")
+    
+    print("Grab the " + adjective + " " + noun + " and " + verb + " to the " + place + ".")
+    
+
+main()
+
+    
+    
