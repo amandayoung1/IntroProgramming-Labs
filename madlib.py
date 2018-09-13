8681e7f6858a87ad2dc6092710fe3313bb5a0aec
amandayoung1 <amanda.young@marist.edu>
2018-09-13 14:46:47 -0400

    Second solution to Mad Libs Exercise

--- madlib.py
+++ madlib.py
@@ -3,15 +3,20 @@
 # Date: 9/13/2018
 # Mad Lib Program for Lab Exercise
 
-def main ():
+def promptForWords(): 
+    global noun, verb, adjective, place
     noun = input("Enter a noun: ")
     verb = input("Enter a verb: ")
     adjective = input("Enter an adjective: ")
     place = input("Enter a place: ")
     print("\n")
-    
+
+def makeAndPrintSentence():
     print("Grab the " + adjective + " " + noun + " and " + verb + " to the " + place + ".")
     
+def main():
+    promptForWords()
+    makeAndPrintSentence()
 
 main()
 
