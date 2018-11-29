304d94b7f0286655bec2a94b74ef3dceefccc122
amandayoung1 <amanda.young1@marist.edu>
2018-11-29 15:06:53 -0500

    Added a product class to the robot store program.

--- RobotStore.py
+++ RobotStore.py
@@ -1,23 +1,30 @@
+# Amanda Young
+# Lab #9
+# 11/29/2018
 
 
-productNames = [ "Ultrasonic range finder"
-                , "Servo motor"
-                , "Servo controller"
-                , "Microcontroller Board"
-                , "Laser range finder"
-                , "Lithium polymer battery"
-                ]
+class Product:
+    def __init__ (self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity        
 
-productPrices = [ 2.50, 14.99, 44.95, 34.95, 149.99, 8.99 ]
-productQuantities = [ 4, 10, 5, 7, 2, 8 ]
+
+products = [ Product("Ultrasonice range finder", 2.50, 4),
+             Product("Servo motor", 14.99, 10),
+             Product("Servo controller", 44.95, 5),
+             Product("Microcontroller Board", 34.95, 7),
+             Product("Laser range finder", 149.99, 2),
+             Product("Lithium polymer battery", 8.99, 8)]
+
 
 def printStock():
     print()
     print("Available Products")
     print("------------------")
-    for i in range(0,len(productNames)):
-        if productQuantities[i] > 0:
-            print(str(i)+")",productNames[i], "$", productPrices[i])
+    for i in range(0,len(products)):
+        if products[i].quantity > 0:
+            print(str(i)+")",products[i].name, "$", products[i].price)
     print()
     
 def main():
@@ -33,15 +40,15 @@ def main():
         prodId = int(vals[0])
         count = int(vals[1])
 
-        if productQuantities[prodId] >= count:
-            if cash >= productPrices[prodId] * count:
-                productQuantities[prodId] -= count
-                cash -= productPrices[prodId] * count
-                print("You purchased", count, productNames[prodId]+".")
+        if products[prodId].quantity >= count:
+            if cash >= products[prodId].price * count:
+                products[prodId].quantity -= count
+                cash -= products[prodId].price * count
+                print("You purchased", count, products[prodId].name + ".")
                 print("You have $", "{0:.2f}".format(cash), "remaining.")
             else:
                 print("Sorry, you cannot afford that product.")
         else:
-            print("Sorry, we are sold out of", productNames[prodId])
-            
-main()
+            print("Sorry, we are sold out of", products[prodId].name)
+
+main()
